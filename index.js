dca2806e7c8c2c3a98a2bb5d9810ef80f81ece6a
Jakhongir <odilovjahongir2001@gmail.com>
2026-03-10 18:23:49 +0500

    request

--- index.js
+++ index.js
@@ -1,23 +1,87 @@
-const memory = require("os");
-const folder = require("fs");
+/**Import modules */
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const app = express();
 
-const EventEmitter = require("events");
-class MyEmitter extends EventEmitter {}
-const emitter = new MyEmitter();
+/**Variables */
 
-emitter.on("message", (arg) => {
-	console.log("Event Listener.....", arg);
+let users = [
+	{
+		id: 1,
+		firs_name: "Temurbek",
+		last_name: "To'xtasinov",
+		age: 20,
+		job: "frontend",
+	},
+	{
+		id: 2,
+		firs_name: "Jahongir",
+		last_name: "To'xtasinov",
+		age: 20,
+		job: "pupil",
+	},
+	{
+		id: 3,
+		firs_name: "Ali",
+		last_name: "To'xtasinov",
+		age: 22,
+		job: "teacher",
+	},
+	{
+		id: 4,
+		firs_name: "Vali",
+		last_name: "To'xtasinov",
+		age: 20,
+		job: "cook",
+	},
+	{
+		id: 5,
+		firs_name: "Alisher",
+		last_name: "To'xtasinov",
+		age: 20,
+		job: "cook",
+	},
+];
+let id = 1;
+
+/** Request and Response Methods */
+
+app.use(express.json());
+
+/** FILTER  USERS */
+app.get("/users/query", (req, res) => {
+	const filteredUsers = users.filter((user) => user.job === req.query.job);
+	res.send(filteredUsers);
 });
-emitter.emit("message", { id: 1, url: "https://google.com" });
+/** GET ALL USERS */
 
-// console.log(memory.freemem()); //71581696 BIT
-// console.log(memory.platform());
-// console.log(memory.totalmem()); //8589934592 BIT
+app.get("/users", (req, res) => {
+	res.send(users);
+});
 
-// folder.readFile("index.js", (err, file) => {
-// 	if (err) {
-// 		console.log(err);
+/** POST NEW USER */
+
+app.post("/users", (req, res) => {
+	const user = { id: id++, firs_name: req.body.firs_name, last_name: req.body.last_name, age: req.body.age, job: req.body.job };
+	users.push(user);
+	res.status(201).send(user);
+});
+
+/** GET SINGLE USER */
+// app.get("/users/:id", (req, res) => {
+// 	const user = users.find((user) => user.id === parseInt(req.params.id));
+
+// 	if (user) {
+// 		res.send(user);
 // 	} else {
-// 		console.log(file);
+// 		res.status(404).json({ message: "Foydalanuvchi topilmadi" });
 // 	}
 // });
+
+/** DELETE SINGLE USER */
+
+/** EDIT SINGLE USER */
+
+/** LISTEN PORT */
+
+app.listen(3000, () => console.log("Server is running"));
